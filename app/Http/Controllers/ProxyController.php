4102cb414002ed857e65ed92f102b1c0c270207c
Henrique Mouta <henrique@mowta.pt>
2015-10-31 20:08:39 +0000

    Raised limit to 20 proxies

--- app/Http/Controllers/ProxyController.php
+++ app/Http/Controllers/ProxyController.php
@@ -39,43 +39,13 @@ class ProxyController extends BaseController {
 		"103.225.66.138:3128",
 	];
 
-	protected function checkProxies() {
-		$mc = curl_multi_init ();
-		for ($thread_no = 0; $thread_no<count($proxies); $thread_no++)
-		{
-			$c [$thread_no] = curl_init ();
-			curl_setopt ($c [$thread_no], CURLOPT_URL, "http://google.com");
-			curl_setopt ($c [$thread_no], CURLOPT_HEADER, 0);
-			curl_setopt ($c [$thread_no], CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt ($c [$thread_no], CURLOPT_CONNECTTIMEOUT, 5);
-			curl_setopt ($c [$thread_no], CURLOPT_TIMEOUT, 10);
-			curl_setopt ($c [$thread_no], CURLOPT_PROXY, trim ($proxies [$thread_no]));
-			curl_setopt ($c [$thread_no], CURLOPT_PROXYTYPE, 0);
-			curl_multi_add_handle ($mc, $c [$thread_no]);
-		}
-
-		do {
-			while (($execrun = curl_multi_exec ($mc, $running)) == CURLM_CALL_MULTI_PERFORM);
-			if ($execrun != CURLM_OK) break;
-			while ($done = curl_multi_info_read ($mc))
-			{
-				$info = curl_getinfo ($done ['handle']);
-				if ($info ['http_code'] == 301) {
-					echo trim ($proxies [array_search ($done['handle'], $c)])."\r\n";
-				}
-				curl_multi_remove_handle ($mc, $done ['handle']);
-			}
-		} while ($running);
-		curl_multi_close ($mc);
-	}
-
 	public function getProxyList( Request $req ) {
 		return Proxy::all();
 	}
 
 	public function getProxy( Request $req ) {
 
-		$proxy = Proxy::query()->orderBy('speed', 'asc')->take(10)->get()->shuffle()->first();
+		$proxy = Proxy::query()->orderBy('speed', 'asc')->take(20)->get()->shuffle()->first();
 
 		return $proxy;
 	}
